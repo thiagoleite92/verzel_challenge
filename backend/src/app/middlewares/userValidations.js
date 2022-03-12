cf31956241a84c588b6fd15453ea53569b783af2
Thiago Leite <leite.tjs@gmail.com>
2022-03-12 14:34:02 -0300

    feat: add email validation to userlogin

--- backend/src/app/middlewares/userValidations.js
+++ backend/src/app/middlewares/userValidations.js
@@ -1,6 +1,13 @@
+const validateEmail = (email) => {
+  const validEmail = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.([a-z]+))?$/i;
+
+  const verifyEmail = validEmail.test(email);
+
+  return verifyEmail ? true : false;
+};
+
 const loginDataValidation = (req, res, next) => {
   const fields = ['email', 'password'];
-  const validEmail = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.([a-z]+))?$/i;
 
   const { body } = req;
 
@@ -10,12 +17,16 @@ const loginDataValidation = (req, res, next) => {
     }
   }
 
-  if (!validEmail.test(body.email)) {
-    return res.status(400).json({ message: 'Invalid email'});
+  if (!validateEmail(body.email)) {
+    return res.status(400).json({ message: 'Invalid email address'});
   }
 
   next();
 };
+
+// const registerDataValidation = (req, res, next) => {
+//   const fields
+// }
 
 module.exports = {
   loginDataValidation,
