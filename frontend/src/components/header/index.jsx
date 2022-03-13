3dcbda8a0046bd172574fb223dd9389f21041fde
Thiago Leite <leite.tjs@gmail.com>
2022-03-13 19:51:15 -0300

    chore: add styled-components

---
+++ frontend/src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <>
+      <div>Header</div>
+      <div>header</div>
+    </>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <>
-      <Header />
+      <div>oi</div>
     </>);
 }
 
