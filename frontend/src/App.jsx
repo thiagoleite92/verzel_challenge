8cc11fe908c89b8ca585fccc948e82e0b4599f5d
Thiago Leite <leite.tjs@gmail.com>
2022-03-13 19:04:14 -0300

    chore: add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages';
 
 function App() {
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
