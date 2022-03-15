2beea52321ef49cd61f908391718a95b63729efe
Thiago Leite <leite.tjs@gmail.com>
2022-03-14 21:14:02 -0300

    feat: add page to render lecture by id

--- frontend/src/components/uniquelecture/Lecture.jsx
+++ frontend/src/components/uniquelecture/Lecture.jsx
@@ -1,8 +1,31 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-function Lecture() {
+function Lecture({ lectureById }) {
   return (
-    <div>Lecture</div>
+    <section>
+      <h2>
+        {lectureById.lecture}
+      </h2>
+      <h3>
+        Date:
+        {' '}
+        {lectureById.startDate}
+      </h3>
+      <h4>
+        Module:
+        {' '}
+        {lectureById.moduleId}
+      </h4>
+      <h4>What are you going to learn?</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quam earum enim sunt. Ullam tempora unde molestias doloremque, eius aspernatur
+        dolorem ratione nulla at, ut facere iste dolorum cum doloribus corrupti.
+      </p>
+      <button type="button">teste</button>
+      <button type="button">teste</button>
+    </section>
   );
 }
 
